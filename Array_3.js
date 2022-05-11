61cc1d268bdaf99b8d51a1123a80cd404ebee18f
Izan Arraez <resinpch@gmail.com>
2022-05-11 08:49:11 +0200

    PrimerCommit

---
+++ Array_3.js
@@ -0,0 +1,32 @@
+function array3(){
+    let a = [];
+    let b = [];
+    let c = [];
+    let cont = 0;
+
+    for(let i=0;i<10;i++){
+        a[i] = parseInt(Math.random()*(10-0)+0);
+        document.write(a[i]);
+         cont++;
+    }
+        
+    document.write("<br>");
+        
+    for(let i=0;i<10;i++){
+        b[i] = parseInt(Math.random()*(10-0)+0);
+        document.write(b[i]);
+        cont++;
+    }
+    document.write("<br>");
+
+    document.write(cont);
+
+    document.write("<br>");
+
+    for(let i=0;i<a.length-1;i++){
+        let elemento = c[i];
+
+        if(!elemento.includes())
+    }
+
+}
